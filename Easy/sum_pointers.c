8281a27548fa6dd7c4c726640f73b5edf9a4cdd6
Anthony-Pius-1729 <tonypius1729@gmail.com>
2025-02-19 03:27:28 +0300

    sum and diff with pointers

---
+++ Easy/sum_pointers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+/*
+Task
+
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. 
+Set the value of  to their sum, and  to their absolute difference. 
+There is no return value, and no return statement is needed.
+
+*/
+void update(int *a,int *b) {
+    
+    int sum;
+    int diff;
+    
+    sum = *a + *b;
+    diff = abs(*a - *b);
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
