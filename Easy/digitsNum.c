47f51d2a8dc9b22aa7a0acc425ceda68909ed8ca
Anthony-Pius-1729 <tonypius1729@gmail.com>
2025-03-10 17:22:31 +0300

    count digits

---
+++ Easy/digitsNum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int digits = 0, n;
+
+    printf("Enter a number");
+
+    scanf("%d", &n);
+
+    do {
+        n/=10;
+        digits++;
+    }while (n !=0);
+
+    printf("The number has %d digits", digits);
+
+}
